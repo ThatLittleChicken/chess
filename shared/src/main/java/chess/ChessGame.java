a07c76c19ac8101bfb8510625153657e42d2c25d
genthoongyong <yonggenthoong@gmail.com>
2024-07-12 00:50:31 -0600

    Stalemate Check

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -165,7 +165,19 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)) {
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    if (validMoves(new ChessPosition(i, j), teamColor) != null &&
+                            !validMoves(new ChessPosition(i, j), teamColor).isEmpty()) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
