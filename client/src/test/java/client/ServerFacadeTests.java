7750919b91f753554e75839bcaa99c1bc452851a
genthoongyong <yonggenthoong@gmail.com>
2024-08-05 19:36:55 -0600

    Game tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -87,5 +87,68 @@ public class ServerFacadeTests {
         assertThrows(Exception.class, () -> facade.logout(""), "Should throw on empty token");
     }
 
+    @Test
+    void createGame() throws Exception {
+        var rr = facade.register(RR);
+        var cr = new CreateRequest("game1");
+        assertDoesNotThrow(() -> facade.createGame(cr, rr.authToken()), "Should not throw on create game");
+    }
 
+    @Test
+    void createGameError() throws Exception {
+        var rr = facade.register(RR);
+        var cr = new CreateRequest("");
+        assertThrows(Exception.class, () -> facade.createGame(cr, rr.authToken()), "Should throw on empty game name");
+        assertThrows(Exception.class, () -> facade.createGame(cr, "badtoken"), "Should throw on bad token");
+    }
+
+    @Test
+    void listGames() throws Exception {
+        var rr = facade.register(RR);
+        var cr = new CreateRequest("game1");
+        facade.createGame(cr, rr.authToken());
+        assertDoesNotThrow(() -> facade.listGames(rr.authToken()), "Should not throw on list games");
+        ListResult lr2 = facade.listGames(rr.authToken());
+        assertFalse(lr2.games().isEmpty(), "Should have at least one game");
+        assertEquals("game1", lr2.games().stream().filter(g -> g.gameName().equals("game1")).findFirst().get().gameName(),
+                "Should have game1");
+    }
+
+    @Test
+    void listGamesError() {
+        assertThrows(Exception.class, () -> facade.listGames("badtoken"), "Should throw on bad token");
+        assertThrows(Exception.class, () -> facade.listGames(""), "Should throw on empty token");
+    }
+
+    @Test
+    void joinGame() throws Exception {
+        var rr = facade.register(RR);
+        var cr = new CreateRequest("game1");
+        facade.createGame(cr, rr.authToken());
+        ListResult lr2 = facade.listGames(rr.authToken());
+        var gameID = lr2.games().stream().filter(g -> g.gameName().equals("game1")).findFirst().get().gameID();
+        JoinRequest jr = new JoinRequest(gameID, "WHITE");
+        assertDoesNotThrow(() -> facade.joinGame(jr, rr.authToken()), "Should not throw on join game");
+        ListResult lr3 = facade.listGames(rr.authToken());
+        assertEquals("player1", lr3.games().stream().filter(g -> g.gameID() == gameID).findFirst().get().whiteUsername(),
+                "Should have player1 as white");
+    }
+
+    @Test
+    void joinGameError() throws Exception {
+        var rr = facade.register(RR);
+        var cr = new CreateRequest("game1");
+        facade.createGame(cr, rr.authToken());
+        ListResult lr2 = facade.listGames(rr.authToken());
+        var gameID = lr2.games().stream().filter(g -> g.gameName().equals("game1")).findFirst().get().gameID();
+        JoinRequest jr = new JoinRequest(gameID, "WHITE");
+        assertThrows(Exception.class, () -> facade.joinGame(jr, "badtoken"), "Should throw on bad token");
+        JoinRequest jr2 = new JoinRequest(0, "WHITE");
+        assertThrows(Exception.class, () -> facade.joinGame(jr2, rr.authToken()), "Should throw on bad game ID");
+        JoinRequest jr3 = new JoinRequest(gameID, "BADCOLOR");
+        assertThrows(Exception.class, () -> facade.joinGame(jr3, rr.authToken()), "Should throw on bad color");
+        JoinRequest jr4 = new JoinRequest(gameID, "WHITE");
+        facade.joinGame(jr4, rr.authToken());
+        assertThrows(Exception.class, () -> facade.joinGame(jr, rr.authToken()), "Should throw on taken color");
+    }
 }
