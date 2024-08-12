aacfa104ed5ecb9d83e958dc65d028a9936a2a32
genthoongyong <yonggenthoong@gmail.com>
2024-08-12 03:40:13 -0600

    Highlight valid moves

--- client/src/main/java/ui/BoardUI.java
+++ client/src/main/java/ui/BoardUI.java
@@ -4,15 +4,38 @@ import chess.ChessGame;
 import chess.ChessPosition;
 import model.GameData;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BoardUI {
     public BoardUI() {
     }
 
-    public String drawBoard(GameData gameData) {
-        return "\n" + drawBlackBoard(gameData.game()) + "\n\n" + drawWhiteBoard(gameData.game());
+    public String drawBoard(ChessGame game, ChessGame.TeamColor color) {
+        if (color.equals(ChessGame.TeamColor.WHITE)) {
+            return drawWhiteBoard(game, null);
+        } else {
+            return drawBlackBoard(game, null);
+        }
     }
 
-    public String drawWhiteBoard(ChessGame game) {
+    public String drawHighlightedBoard(ChessGame game, ChessPosition position, ChessGame.TeamColor color) {
+        Set<ChessPosition> validPos = new HashSet<>();
+        game.validMoves(position).forEach(move -> {
+            if (color == null) {
+                validPos.add(move.getEndPosition());
+            } else if (game.getBoard().getPiece(move.getStartPosition()).getTeamColor().equals(color)) {
+                validPos.add(move.getEndPosition());
+            }
+        });
+        if (color == null || color.equals(ChessGame.TeamColor.WHITE)) {
+            return drawWhiteBoard(game, validPos);
+        } else {
+            return drawBlackBoard(game, validPos);
+        }
+    }
+
+    private String drawWhiteBoard(ChessGame game, Set<ChessPosition> validPos) {
         StringBuilder result = new StringBuilder();
         result.append(EscapeSequences.SET_TEXT_COLOR_BLACK);
         result.append(String.format("%s%s a  b  c  d  e  f  g  h %s%s\n",
@@ -20,7 +43,18 @@ public class BoardUI {
         for (int i = 8; i >= 1; i--) {
             result.append(String.format("%s %s ", EscapeSequences.SET_BG_COLOR_LIGHT_BROWN, i));
             for (int j = 1; j <= 8; j++) {
-                var bgSquare = ((i+j) % 2 == 0) ? EscapeSequences.SET_BG_COLOR_BROWN : EscapeSequences.SET_BG_COLOR_BEIGE;
+                String bgSquare;
+                if (validPos != null && validPos.contains(new ChessPosition(i, j))) {
+                    if ((i+j) % 2 == 0) {
+                        bgSquare = EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+                    } else {
+                        bgSquare = EscapeSequences.SET_BG_COLOR_GREEN;
+                    }
+                } else if ((i+j) % 2 == 0) {
+                    bgSquare = EscapeSequences.SET_BG_COLOR_BROWN;
+                } else {
+                    bgSquare = EscapeSequences.SET_BG_COLOR_BEIGE;
+                }
                 result.append(String.format("%s%s", bgSquare, pieceSymbol(game, i, j)));
             }
             result.append(String.format("%s %s %s\n", EscapeSequences.SET_BG_COLOR_LIGHT_BROWN, i, EscapeSequences.RESET_BG_COLOR));
@@ -30,7 +64,7 @@ public class BoardUI {
         return result.toString();
     }
 
-    public String drawBlackBoard(ChessGame game) {
+    public String drawBlackBoard(ChessGame game, Set<ChessPosition> validPos) {
         StringBuilder result = new StringBuilder();
         result.append(EscapeSequences.SET_TEXT_COLOR_BLACK);
         result.append(String.format("%s%s h  g  f  e  d  c  b  a %s%s\n",
@@ -38,7 +72,18 @@ public class BoardUI {
         for (int i = 1; i <= 8; i++) {
             result.append(String.format("%s %s ", EscapeSequences.SET_BG_COLOR_LIGHT_BROWN, i));
             for (int j = 8; j >= 1; j--) {
-                var squareBg = ((i+j) % 2 != 0) ? EscapeSequences.SET_BG_COLOR_BEIGE : EscapeSequences.SET_BG_COLOR_BROWN;
+                String squareBg;
+                if (validPos != null && validPos.contains(new ChessPosition(i, j))) {
+                    if ((i+j) % 2 != 0) {
+                        squareBg = EscapeSequences.SET_BG_COLOR_GREEN;
+                    } else {
+                        squareBg = EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+                    }
+                } else if ((i+j) % 2 != 0) {
+                    squareBg = EscapeSequences.SET_BG_COLOR_BEIGE;
+                } else {
+                    squareBg = EscapeSequences.SET_BG_COLOR_BROWN;
+                }
                 result.append(String.format("%s%s", squareBg, pieceSymbol(game, i, j)));
             }
             result.append(String.format("%s %s %s\n", EscapeSequences.SET_BG_COLOR_LIGHT_BROWN, i, EscapeSequences.RESET_BG_COLOR));
@@ -48,7 +93,7 @@ public class BoardUI {
         return result.toString();
     }
 
-    public String pieceSymbol(ChessGame game, int row, int col) {
+    private String pieceSymbol(ChessGame game, int row, int col) {
         var chessPiece = game.getBoard().getPiece(new ChessPosition(row, col));
         if (chessPiece == null) {
             return EscapeSequences.EMPTY;
